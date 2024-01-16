0a7b1fac5f9f6bd2d42df146d5e035673a1bc031
mzkwcim <wkrak98@gmail.com>
2024-01-16 17:06:03 +0100

    Dodaj pliki projektów.

---
+++ TestWraperClie/Program.cs
@@ -0,0 +1,8 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+
+var powerShellResults = await Cli.Wrap("Powershell")
+    .WithArguments("foreach($dir in (ls C:\\Users\\Laptop\\Desktop\\UdaloSie).FullName ) \r\n{ \r\n    $dir\r\n    (get-acl $dir).Access | Select-Object -expandproperty Identityreference | Format-Table -HideTableHeaders  \r\n}")
+    .ExecuteBufferedAsync();
+
+Console.WriteLine(powerShellResults.StandardOutput);
